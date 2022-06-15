368103ceb04da446ff965421a977e7f2533ba090
Ariel Lima <72455661+whyariel2@users.noreply.github.com>
2022-06-15 17:00:01 -0300

    update services

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+  // FIXED HEADER
+  window.onscroll = function () {
+    if (window.pageYOffset > 140) {
+      $("#header").addClass("sticky-top");
+    } else {
+      $("#header").removeClass("sticky-top");
+    }
+  };
+});
