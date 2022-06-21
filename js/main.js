3bd40424200998df214978eece12fcc1e38c6776
Ariel Lima <72455661+whyariel2@users.noreply.github.com>
2022-06-20 23:13:05 -0300

    all done

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ jQuery(document).ready(function ($) {
   // FIXED HEADER
   window.onscroll = function () {
     if (window.pageYOffset > 140) {
-      $("#header").addClass("sticky-top");
+      $("header").addClass("sticky-top");
     } else {
-      $("#header").removeClass("sticky-top");
+      $("header").removeClass("sticky-top");
     }
   };
 });
